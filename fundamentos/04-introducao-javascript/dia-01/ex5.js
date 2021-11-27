c8d6aeeb7f273e3a87b1a9fa8ca6eef36392e86a
Eduardo <elkadu@live.com>
2021-11-27 00:25:31 -0300

    exercicios/4.1

--- fundamentos/04-introducao-javascript/dia-01/ex5.js
+++ fundamentos/04-introducao-javascript/dia-01/ex5.js
@@ -1,6 +1,14 @@
-const a=56, b=75, c=49
+const a=56, b=47, c=-77
 var tri= a+b+c
 
-if(tri==180){
-    console.log('É um triangulo')
+if (a<0 || b<0 || c<0){
+    console.log('Angulo invalido')
+}
+else{
+    if(tri==180){
+        console.log(true)
+    }
+    else {
+        console.log(false)
+    }
 }
